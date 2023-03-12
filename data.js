cebdc17a11d7904f1692a25ab5edc64f146f95b1
chaowen <chaowen@mpi-sws.org>
2023-03-12 12:05:51 +0100

    add prev and next button

---
+++ data.js
@@ -0,0 +1,18 @@
+export const taskIds = {
+  chap1: ["1", "9a"], // chap1
+  chap2: ["10a", "11", "12a", "14", "17a", "18b"], // chap2
+  chap3: ["20", "23", "24", "26", "29"], // chap3
+  chap4: ["30", "34", "39"], //chap4
+  chap6: ["57", "58"], //chap6
+  chap7: ["61", "62", "65"], //chap7
+  chap8: ["71", "73"], // chap8
+  chap9: ["84", "85", "87"], //chap9
+  chap10: ["91", "93"], //chap10
+  chap11: ["103", "104"], // chap11
+};
+
+export const allIds = Object.values(taskIds).flat();
+
+export const pracIds = [1, 2, 3, 4, 5, 6];
+// export const pracIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+export const baseUrl = "xlogomini_practice.html";
